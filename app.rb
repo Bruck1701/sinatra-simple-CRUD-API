56ea17a60f3e7b534c7f216a90aa0d71e0d72d6f
Bruck1701 <kummel@gmail.com>
2020-08-17 23:29:39 +0200

    add: add: models and connection to db

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
+# something
 require 'sinatra'
 require 'sinatra/activerecord'
-require './config/environments' #db configurations
-
+require './config/environments' # db configurations
+require './models/model'
 
 get '/' do
   'Hello from sinatra!'
 end
-
---
+++ db/migrate/20200817212822_create_model.rb
@@ -0,0 +1,4 @@
+class CreateModel < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
